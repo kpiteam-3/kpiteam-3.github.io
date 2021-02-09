b07e7eb05ad5ca64a30266045d3108b60acacdcf
Kate Kasiutich <31770149+KateKasiutich@users.noreply.github.com>
2021-02-09 18:50:11 +0200

    Kasiutich js
    check off specific Todos By Clicking added

--- assets/jsPart.js
+++ assets/jsPart.js
@@ -10,3 +10,19 @@ $("input[type='text']").keypress(function(event) {
 $(".fa-edit").click(function() {
     $("input[type='text']").fadeToggle();
 })
+
+// check off specific Todos By Clicking
+$("ul").on("click", "li", function() {
+    if($(this).css("color") === "rgb(128, 128, 128)") {
+        $(this).css({
+            color: "#666",
+            textDecoration: "none"
+        })
+    }
+   else { 
+       $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    });
+}
+});
