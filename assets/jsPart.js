671e167dfbecd45f802738cbe3424a19b4a47141
aleksandrovychkatya <48254014+aleksandrovychkatya@users.noreply.github.com>
2021-02-09 20:43:47 +0200

    Update jsPart.js
    Added function click on X

--- assets/jsPart.js
+++ assets/jsPart.js
@@ -26,3 +26,11 @@ $("ul").on("click", "li", function() {
     });
 }
 });
+
+//click on X to delete Todo
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
